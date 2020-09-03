675d08004244b07556bafc00b21116bc1148445e
little-pear <2437824607@qq.com>
2020-09-04 01:12:51 +0800

    登录UI

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Login from "../views/Login.vue";
 import Main from "../views/Main.vue";
 import CategoryEdit from "../views/CategoryEdit.vue";
 import CategoryList from "../views/CategoryList.vue";
@@ -13,9 +14,20 @@ import HeroList from "../views/HeroList.vue";
 import ArticleEdit from "../views/ArticleEdit.vue";
 import ArticleList from "../views/ArticleList.vue";
 
+import AdEdit from "../views/AdEdit.vue";
+import AdList from "../views/AdList.vue";
+
+import AdminUserEdit from "../views/AdminUserEdit.vue";
+import AdminUserList from "../views/AdminUserList.vue";
+
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
   {
     path: "/",
     name: "main",
@@ -75,6 +87,34 @@ const routes = [
       {
         path: "/articles/list",
         component: ArticleList,
+      },
+
+      {
+        path: "/ads/create",
+        component: AdEdit,
+      },
+      {
+        path: "/ads/edit/:id",
+        component: AdEdit,
+        props: true,
+      },
+      {
+        path: "/ads/list",
+        component: AdList,
+      },
+
+      {
+        path: "/admin_users/create",
+        component: AdminUserEdit,
+      },
+      {
+        path: "/admin_users/edit/:id",
+        component: AdminUserEdit,
+        props: true,
+      },
+      {
+        path: "/admin_users/list",
+        component: AdminUserList,
       },
     ],
   },
